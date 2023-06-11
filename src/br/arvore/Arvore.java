24c415cae27202a7de94e006851d8cd4a0bc250f
Leobritt <leobritto160@gmail.com>
2023-06-11 18:17:56 -0300

    Adição do método pré ordem, pós ordem e Em ordem

--- src/br/arvore/Arvore.java
+++ src/br/arvore/Arvore.java
@@ -1,57 +1,103 @@
 package br.arvore;
 
 public class Arvore<TIPO extends Comparable> {
-	
+	/*
+	 * Em ordem 1- esquerda, 2- cima, 3- direita OBS: se tiver filho repete
+	 */
+
 	private Elemento<TIPO> raiz;
 
 	public Arvore() {
 		this.raiz = null;
 	}
-	
+
 	public void add(TIPO valor) {
 		Elemento<TIPO> novoElemento = new Elemento<TIPO>(valor);
-		
+
 		if (raiz == null) {
 			this.raiz = novoElemento;
-		}else {
+		} else {
 			Elemento<TIPO> elementoAtual = this.raiz;
-			
+
 			while (true) {
-				/*Comparação se é menor que o atual
-				 *Menor que o atual vai para esquerda*/
-				/*Método compareTo() retorna 1 qndo é maior, 0 qndo é igual ou -1 qndo é menor*/
+				/*
+				 * Comparação se é menor que o atual Menor que o atual vai para esquerda
+				 */
+				/*
+				 * Método compareTo() retorna 1 qndo é maior, 0 qndo é igual ou -1 qndo é menor
+				 */
 				if (novoElemento.getValor().compareTo(elementoAtual.getValor()) == -1) {
-					/*atual tem filho na esquerda?*/
+					/* atual tem filho na esquerda? */
 					if (elementoAtual.getEsquerda() != null) {
-						/*descendo para esquerda*/
+						/* descendo para esquerda */
 						elementoAtual = elementoAtual.getEsquerda();
-					}else {
-						/*se tiver vazio ele irá se tornar o elemento da esquerda*/
+					} else {
+						/* se tiver vazio ele irá se tornar o elemento da esquerda */
 						elementoAtual.setEsquerda(novoElemento);
 						break;
 					}
-				
+
 				}
-				/*Maior ou igual que o atual vai para direita*/
+				/* Maior ou igual que o atual vai para direita */
 				else {
-					/*atual tem filho na direita*/
+					/* atual tem filho na direita */
 					if (elementoAtual.getDireita() != null) {
-						/*descendo para direita*/
+						/* descendo para direita */
 						elementoAtual = elementoAtual.getDireita();
-					}else {
-						/*se tiver vazio ele irá se tornar o elemento da direita*/
+					} else {
+						/* se tiver vazio ele irá se tornar o elemento da direita */
 						elementoAtual.setDireita(novoElemento);
 						break;
 					}
 				}
 			}
 		}
-		
+
 	}
 
-	@Override
-	public String toString() {
-		return "Arvore [raiz=" + raiz + "]";
+	public Elemento<TIPO> getRaiz() {
+		return raiz;
 	}
-	
+
+	public void addEsquerda(TIPO valor) {
+		Elemento<TIPO> novoElemento = new Elemento<TIPO>(valor);
+
+		if (raiz == null) {
+			this.raiz = novoElemento;
+		} else {
+			Elemento<TIPO> elementoAtual = this.raiz;
+
+			while (true) {
+
+			}
+		}
+	}
+
+	public void emOrdem(Elemento<TIPO> atual) {
+		if (atual != null) {
+			emOrdem(atual.getEsquerda());
+			System.out.println(atual.getValor());
+			emOrdem(atual.getDireita());
+		}
+
+	}
+
+	public void preOrdem(Elemento<TIPO> atual) {
+		if (atual != null) {
+			System.out.println(atual.getValor());
+			preOrdem(atual.getEsquerda());
+			preOrdem(atual.getDireita());
+		}
+
+	}
+
+	public void posOrdem(Elemento<TIPO> atual) {
+		if (atual != null) {
+			posOrdem(atual.getEsquerda());
+			posOrdem(atual.getDireita());
+			System.out.println(atual.getValor());
+
+		}
+
+	}
 }
--- src/br/arvore/Main.java
+++ src/br/arvore/Main.java
@@ -3,9 +3,23 @@ package br.arvore;
 public class Main {
 	public static void main(String[] args) {
 		Arvore<Integer> arvore = new Arvore<Integer>();
-		arvore.add(2);
+		arvore.add(10);
+		arvore.add(8);
+		arvore.add(5);		
+		arvore.add(9);
+		arvore.add(7);
+		arvore.add(18);
+		arvore.add(13);
 		arvore.add(20);
 		
-		System.out.println(arvore);
+		System.out.println("\nEm ordem");
+		arvore.emOrdem(arvore.getRaiz());
+		
+		
+		System.out.println("\nPré ordem");
+		arvore.preOrdem(arvore.getRaiz());
+		
+		System.out.println("\nPos ordem");
+		arvore.posOrdem(arvore.getRaiz());
 	}
 }
